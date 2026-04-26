ad68115afe97fea58348e0047df665525c52aecc
Nico Felbinger <nico@felbinger.eu>
2026-04-26 18:52:54 +0200

    checks/epc+enb: split into two machines

--- checks/epc+enb/default.nix
+++ checks/epc+enb/default.nix
@@ -6,80 +6,97 @@
 {
   name = "epc+enb";
 
-  node.pkgsReadOnly = false;
+  defaults = {
+    # remove default ip addresses from interface
+    virtualisation.interfaces.eth1 = {
+      vlan = 1;
+      assignIP = false;
+    };
 
-  nodes.machine = {
-    imports = [
-      self.nixosModules.default
-    ];
-    services.srsran = {
-      enable = true;
-      epc = {
+    networking.firewall.enable = false;
+
+    imports = [ self.nixosModules.default ];
+    services.srsran.enable = true;
+  };
+
+  nodes = {
+    epc = {
+      networking.interfaces.eth1.ipv4.addresses = [
+        {
+          address = "192.0.2.10";
+          prefixLength = 24;
+        }
+      ];
+      services.srsran.epc = {
         enable = true;
         settings = {
-          hss.db_file = pkgs.writeText "user_db.csv" ''
-            #
-            # .csv to store UE's information in HSS
-            # Kept in the following format: "Name,Auth,IMSI,Key,OP_Type,OP/OPc,AMF,SQN,QCI,IP_alloc"
-            #
-            # Name:     Human readable name to help distinguish UE's. Ignored by the HSS
-            # Auth:     Authentication algorithm used by the UE. Valid algorithms are XOR
-            #           (xor) and MILENAGE (mil)
-            # IMSI:     UE's IMSI value
-            # Key:      UE's key, where other keys are derived from. Stored in hexadecimal
-            # OP_Type:  Operator's code type, either OP or OPc
-            # OP/OPc:   Operator Code/Cyphered Operator Code, stored in hexadecimal
-            # AMF:      Authentication management field, stored in hexadecimal
-            # SQN:      UE's Sequence number for freshness of the authentication
-            # QCI:      QoS Class Identifier for the UE's default bearer.
-            # IP_alloc: IP allocation stratagy for the SPGW.
-            #           With 'dynamic' the SPGW will automatically allocate IPs
-            #           With a valid IPv4 (e.g. '172.16.0.2') the UE will have a statically assigned IP.
-            #
-            # Note: Lines starting by '#' are ignored and will be overwritten
-            ue1,xor,001010123456789,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,9001,000000001234,7,dynamic
-            ue2,mil,001010123456780,00112233445566778899aabbccddeeff,opc,63bfa50ee6523365ff14c1f45f88737d,8000,000000001234,7,dynamic
-          '';
-          spgw = {
-            # tun interface for the SGi connection
-            sgi_if_addr = "172.16.0.1";
-            sgi_if_name = "srs_spgw_sgi";
-          };
+          hss.db_file = pkgs.writeText "user_db.csv" "";
+          mme.mme_bind_addr = "192.0.2.10";
+          spgw.gtpu_bind_addr = "192.0.2.10";
         };
       };
-      enb = {
+    };
+    enb = {
+      networking.interfaces.eth1.ipv4.addresses = [
+        {
+          address = "192.0.2.11";
+          prefixLength = 24;
+        }
+      ];
+      services.srsran.enb = {
         enable = true;
         settings = {
           enb = {
-            # Local IP address to bind for GTP connection
-            gtp_bind_addr = "127.0.1.1";
-            # Local IP address to bind for S1AP connection
-            s1c_bind_addr = "127.0.1.1";
+            # IP address of MME for S1 connection
+            mme_addr = "192.0.2.10";
+            # Local IP address to bind for GTP/S1AP connection
+            gtp_bind_addr = "192.0.2.11";
+            s1c_bind_addr = "192.0.2.11";
           };
           rf = {
             # use ZeroMQ Virtual Radios
             device_name = "zmq";
-            device_args = "fail_on_disconnect=true,tx_port=tcp://*:2000,rx_port=tcp://localhost:2001,id=enb,base_srate=23.04e6";
+            device_args = "fail_on_disconnect=true,tx_port=tcp://*:2000,rx_port=tcp://192.168.2.11:2001,id=enb,base_srate=23.04e6";
           };
         };
       };
     };
   };
 
-  testScript =
-    { nodes, ... }:
-    let
-      inherit (nodes.machine.services.srsran) epc;
-    in
-    /* python */ ''
-      machine.wait_for_unit("srsran-epc.service")
+  testScript = /* python */ ''
+    start_all()
 
-      machine.wait_until_succeeds("""
-        ip -br a show ${epc.settings.spgw.sgi_if_name} | grep ${epc.settings.spgw.sgi_if_addr}
-      """)
+    def check_log(component: str, line: str):
+      """
+      Utility function to check if the log of an srsran service contains a specified line.
+      """
+      globals()[component].wait_until_succeeds(f"journalctl --boot --unit srsran-{component}.service | grep '{line}'")
 
-      machine.wait_for_unit("srsran-enb.service")
+    with subtest("EPC"):
+      epc.wait_for_unit("network.target")
+      epc.wait_for_unit("srsran-epc.service")
+      check_log("epc", "HSS Initialized")
+      check_log("epc", "MME S11 Initialized")
+      check_log("epc", "MME GTP-C Initialized")
+      check_log("epc", "SPGW GTP-U Initialized")
+      check_log("epc", "SPGW S11 Initialized")
+      check_log("epc", "SP-GW Initialized")
 
-      machine.wait_for_open_port(2000) # port 2000 is defined in zmq device_args
-    '';
+    with subtest("eNodeB"):
+      enb.wait_for_unit("network.target")
+      enb.wait_for_unit("srsran-enb.service")
+      enb.wait_for_open_port(2000) # port 2000 is defined in zmq device_args
+      check_log("enb", "==== eNodeB started ===")
+
+    with subtest("Check if eNodeB has connected to EPC"):
+      check_log("epc", "Received S1 Setup Request")
+      check_log("epc", "S1 Setup Request - eNB Name: srsenb01, eNB id: 0x0")
+      check_log("epc", "S1 Setup Request - MCC:001, MNC:01")
+      check_log("epc", "S1 Setup Request - TAC 1, B-PLMN 0xf110")
+      check_log("epc", "S1 Setup Request - Paging DRX v128")
+      check_log("epc", "Sending S1 Setup Response")
+
+    print(enb.succeed("journalctl -u srsran-enb -n 20"))
+    print(epc.succeed("journalctl -u srsran-epc -n 20"))
+  '';
 }
